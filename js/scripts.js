02c2d4568d074e1bb3ef735b78fbdb3166ef3312
Danny Hartnell <danielhartnell+epicodus@gmail.com>
2015-10-11 17:28:19 -0700

    add css for delete notification. clean up and consolidate jQuery.

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,36 @@
 $(function() {
-	$(".no-things").show();
-  	$(".add-things").hide();
-});
 
-$(function() {
-  $(".no-things form").submit(function(event) {
-  	var username = $("input#username").val();
-  	
-  	$(".username").text(username);
-  	$(".no-things").hide();
-  	$(".add-things").fadeIn();
-  	event.preventDefault();
+  // Set initial div visibility
+  $(".no-things-container").show();
+  $(".add-things-container").hide();
+  $(".confirm-remove").hide();
+
+  // Switch div visibility once user is specified
+  // Make sure that preventDefault() is enabled for submit
+  $(".no-things-container form").submit(function(event) {
+    var username = $("input#username").val();
+
+    $(".username").text(username);
+    $(".no-things-container").hide();
+    $(".add-things-container").fadeIn();
+    event.preventDefault();
   });
-});
+  
+  // Addend new thing to ul
+  // Add logic to remove thing on click
+  $(".add-things-container form").submit(function(event) {
+    var thing = $("input#thing").val();
 
-$(function() {
-	$(".add-things form").submit(function(event) {
-	  var thing = $("input#thing").val();
+    $("ul#thing-entry").prepend("<li>" + "<a href='#' class='thing-object'>" + thing + "</a>" + "</li>");
+    $("input#thing").val('');
 
-	  $("ul#thing-entry").prepend("<li>" + thing + "</li>");
-	  $("input#thing").val('');
-	  $("li").click(function() {
-        $(this).remove();
-      });
-	  event.preventDefault();
-	});
+    $("li").hover(function() {
+      $(this).addClass(".active");
+    });
+
+    $("li").click(function() {
+      $(this).remove();
+    });
+    event.preventDefault();
+  });
 });
